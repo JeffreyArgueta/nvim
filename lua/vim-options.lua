2f85d16805aa872982308e9a61459194b203ec17
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2025-01-17 15:35:33 -0600

    update vim-options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,20 +1,39 @@
+-- General settings
 vim.g.mapleader        = " "
 
 vim.opt.number         = true
 vim.opt.relativenumber = true
-
 vim.opt.splitbelow     = true
 vim.opt.splitright     = true
-
 vim.opt.wrap           = false
-vim.opt.autoindent     = true
 
-vim.opt.expandtab      = true
+-- Identation
+vim.opt.tabstop        = 2
 vim.opt.shiftwidth     = 2
+vim.opt.expandtab      = true
+vim.opt.autoindent     = true
+vim.opt.smartindent    = true
 
--- vim.opt.termguicolors   =   true
+-- UI enhancements
+vim.opt.termguicolors  = true
 vim.opt.scrolloff      = 999
-vim.opt.clipboard      = "unnamedplus"
+vim.opt.list           = true
+vim.opt.listchars      = { tab = "→ ", trail = "·" }
 
-vim.opt.virtualedit    = "block"
-vim.opt.inccommand     = "split"
+
+-- Search improvements
+vim.opt.ignorecase  = true
+vim.opt.smartcase   = true
+vim.opt.hlsearch    = false -- Avoid highlighting all matches
+vim.opt.incsearch   = true  -- Show matches while typing
+
+-- Scrolling and Editing
+vim.opt.clipboard   = "unnamedplus"
+vim.opt.virtualedit = "block"
+vim.opt.inccommand  = "split"
+vim.opt.updatetime  = 300
+
+-- Keybindings
+local keymap        = vim.api.nvim_set_keymap
+keymap("n", "<leader>w", ":w<CR>", { noremap = true })  -- Save file
+keymap("n", "<leader>q", ":q<CR>", { noremap = true })  -- Quit file
