59d59c4c8fc6eaa533d50ca0dca1ef41b0a261a4
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2025-10-17 16:28:54 -0600

    Update: customized LuaLine, added Lua-Vim integration

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,15 +1,45 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = { "rafamadriz/friendly-snippets", "xzbdmw/colorful-menu.nvim" },
   version = "*",
   opts = {
     keymap = { preset = "default" },
     appearance = { nerd_font_variant = "mono" },
     completion = {
-      -- menu = { border = "rounded" },
-      -- documentation = { auto_show = true, window = { border = "rounded" } }
+      documentation = { auto_show = true, window = { border = "rounded" } },
+      menu = {
+        border = "rounded",
+        draw = {
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
+          columns = { { "kind_icon" }, { "label", gap = 1 }, { "kind" } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end
+            }
+          }
+        }
+      }
     },
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      per_filetype = {
+        -- sql = { 'snippets', 'dadbod', 'buffer' },
+        lua = { inherit_defaults = true, 'lazydev' }
+      },
+      providers = {
+        -- dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        lazydev = { name = "LazyDev", module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        }
+      }
+    },
     -- Experimental signature help support
     signature = { enabled = true }
   }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,9 +4,23 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme        = "auto",
-        globalstatus = true, -- Single statusline for all splits
-      }
+        component_separators = { left = '', right = '' },
+        section_separators   = { left = '', right = '' },
+        theme                = "auto",
+        globalstatus         = true, -- Single statusline for all splits
+      },
+      sections = {
+        lualine_a = { { 'mode', icon = "" } },
+        lualine_b = {
+          { 'branch', icon = '' },
+          { 'diff', symbols = { added = " ", modified = " ", removed = " " } },
+          { 'diagnostics', symbols = { error = " ", warn = " ", info = " ", hint = " " }, }
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
     })
   end
 }
