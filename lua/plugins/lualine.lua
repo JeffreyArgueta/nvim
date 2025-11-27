22f78376373366a8ff9516a735a66acf621f7bf5
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2025-11-27 16:24:34 -0600

    Update: lualine & telescope

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -32,12 +32,24 @@ return {
               local extension = vim.fn.expand('%:e') -- file extension
               local icon, _ = require('nvim-web-devicons').get_icon(filename, extension, { default = false })
 
+              -- Check if the current buffer is an Oil.nvim buffer
+              if vim.bo.filetype == 'oil' or require("oil").get_current_dir() then
+                icon = '󰦨'
+                filename = "Oil"
+              end
+
+              -- Check if the current buffer is an Telescope.nvim buffer
+              if vim.bo.filetype == 'TelescopePrompt' then
+                icon = '󰭎'
+                filename = "Telescope"
+              end
+
               -- Status flags
-              local modified = vim.bo.modified and '  ' or ''
-              local readonly = vim.bo.readonly and '  ' or ''
+              local modified = vim.bo.modified and ' ' or ''
+              local readonly = vim.bo.readonly and ' ' or ''
               local status = modified .. readonly
 
-              return string.format('%s %s%s', icon or '', filename, status)
+              return string.format('%s %s %s', icon or '', filename, status)
             end,
             color = { fg = cp.crust, bg = cp.maroon }
           }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,17 +3,33 @@ return {
   tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-ui-select.nvim"
+    "nvim-telescope/telescope-ui-select.nvim",
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
     require("telescope").setup({
-      extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown({}) } }
+      extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown() } },
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = "❯ ",
+        multi_icon = " "
+      }
     })
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope help tags" })
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files,       { desc = "Telescope find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep,        { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers,          { desc = "Telescope buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags,        { desc = "Telescope help tags" })
+    vim.keymap.set("n", "<leader>ft", builtin.treesitter,       { desc = "Telescope treesitter" })
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references,   { desc = "Telescope lsp references" })
+    vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions,  { desc = "Telescope lsp definitions" })
+
+    local nvim_config = function () builtin.find_files { cwd = vim.fn.stdpath("config") } end
+    vim.api.nvim_create_user_command("NeovimConfig", nvim_config, {})
+    vim.keymap.set("n", "<leader>nc", ":NeovimConfig<CR>", { desc = "Telescope nvim .config find files" })
+
     require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("fzf")
   end
 }
